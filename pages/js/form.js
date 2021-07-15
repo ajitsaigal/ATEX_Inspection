31cce819c0086ea86ff5a6bff3b94f6443a10131
ajitsaigal <ajit.saigal@gmail.com>
2021-07-15 20:54:18 +0530

    my first commit

---
+++ pages/js/form.js
@@ -0,0 +1,7 @@
+function active(id){
+    document.getElementById('a').className="opreation";
+    document.getElementById('b').className="opreation";
+    document.getElementById('c').className="opreation";
+    document.getElementById('d').className="opreation";
+    document.getElementById(id).className="active";
+}
